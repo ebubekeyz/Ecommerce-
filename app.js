b5af32a669bef9e42574091f29f9e028c988cc25
ebubekeyz <ebubeofforjoe@gmail.com>
2023-06-19 22:15:35 +0100

    xss

--- app.js
+++ app.js
@@ -1,73 +1,67 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
 
+const express = require('express');
+const app = express();
 
-const express = require('express')
-const app = express()
+const connectDB = require('./db/connect');
 
-const connectDB = require('./db/connect')
+const fileUpload = require('express-fileupload');
 
-const fileUpload = require('express-fileupload')
-
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
-const productRouter = require('./routes/productRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const orderRouter = require('./routes/orderRoutes')
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
-const mongoSanitize = require('express-mongo-sanitize')
+const mongoSanitize = require('express-mongo-sanitize');
 
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
 app.use(
-    rateLimiter({
-        windowMs: 15 * 60 * 1000,
-        max: 60,
-    })
-)
-app.use(helmet())
-app.use(xss())
-app.use(mongoSanitize())
-app.use(express.json())
-app.use(cookieParser(process.env.JWT_SECRET))
-
-app.use(express.static('./public'))
-app.use(fileUpload())
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(express.json());
 const corsOptions = {
-    origin: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-}
-app.use(cors(corsOptions))
+  origin: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser(process.env.JWT_SECRET));
 
+app.use(express.static('./public'));
+app.use(fileUpload());
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/reviews', reviewRouter)
-app.use('/api/v1/orders', orderRouter)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
-
-const port = process.env.PORT || 4200
+const port = process.env.PORT || 4200;
 
 const start = async () => {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => console.log(`Server is listening on port ${port}`))
-}
+  await connectDB(process.env.MONGO_URI);
+  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+};
 
-
-start()
+start();
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,33 +1,30 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const createJWT = ({payload}) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
+const createJWT = ({ payload }) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
 
-    return token
-}
+  return token;
+};
 
-const isTokenValid = ({token}) => jwt.verify(token,process.env.JWT_SECRET)
+const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({res, user}) => {
-const token = createJWT({payload: user})
+const attachCookiesToResponse = ({ res, user }) => {
+  const token = createJWT({ payload: user });
 
-const oneDay = 1000 * 60 * 60 * 24
+  const oneDay = 1000 * 60 * 60 * 24;
 
-res.cookie('token', token, {
-    httpOnly: false,
+  res.cookie('token', token, {
+    httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: true,
-    signed: true
-})
-
-}
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+  });
+};
 
 module.exports = {
-    createJWT,
-    isTokenValid,
-    attachCookiesToResponse
-}
- 
-
-
-
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+};
