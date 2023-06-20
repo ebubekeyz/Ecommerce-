d786457221f93dcdc8e4e026d7006c0513282f3b
ebubekeyz <ebubeofforjoe@gmail.com>
2023-06-20 01:58:08 +0100

    cors

--- utils/jwt.js
+++ utils/jwt.js
@@ -16,8 +16,9 @@ const oneDay = 1000 * 60 * 60 * 24
 res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     signed: true,
+    sameSite: 'none'
 })
 
 }
