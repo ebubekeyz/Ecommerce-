d2d13f964422ebcad6ad82867936e41cb2707459
ebubekeyz <ebubeofforjoe@gmail.com>
2023-06-21 15:53:18 +0100

    commit

--- models/Product.js
+++ models/Product.js
@@ -1,92 +1,90 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 const ProductDescriptionSchema = mongoose.Schema({
-    modelName: { type: String, required: true },
-    quality: { type: String, required: true },
-    material: { type: String, required: true },
-    boxContent: { type: String, required: true },
-    feature: { type: String, required: true },
-    weight: { type: String, required: true },
-    size: { type: String, required: true },
-})
+  modelName: { type: String, required: true },
+  quality: { type: String, required: true },
+  material: { type: String, required: true },
+  boxContent: { type: String, required: true },
+  feature: { type: String, required: true },
+  weight: { type: String, required: true },
+  size: { type: String, required: true },
+});
 
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        required: [true, 'Please provide product name'],
-        maxlength: [100, 'Name can not be more than 100 characters'],
+      type: String,
+      trim: true,
+      required: [true, 'Please provide product name'],
+      maxlength: [100, 'Name can not be more than 100 characters'],
     },
     price: {
-        type: Number,
-        required: [true, 'Please provide product price'],
-        default: 0,
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
     },
     description: [ProductDescriptionSchema],
     image: {
-        type: String,
-        default: '/uploads/example.jpeg',
+      type: String,
+      default: '/uploads/example.jpeg',
     },
     category: {
-        type: String,
-        required: [true, 'Please provide product category'],
-        enum: ['men', 'women'],
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['men', 'women'],
     },
     company: {
-        type: String,
-        required: [true, 'Please provude company'],
-        enum: {
-            values: ['shirt', 'trouser', 'gown'],
-            message: '{VALUE} is not supported',
-        },
+      type: String,
+      required: [true, 'Please provude company'],
+      enum: {
+        values: ['shirt', 'trouser', 'gown'],
+        message: '{VALUE} is not supported',
+      },
     },
     colors: {
-        type: [String],
-        default: ['#222'],
-        required: true,
+      type: [String],
+      default: ['#222'],
+      required: true,
     },
     featured: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     freeShipping: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     inventory: {
-        type: Number,
-        required: true,
-        default: 15,
+      type: Number,
+      required: true,
+      default: 15,
     },
     averageRating: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     numOfReviews: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     user: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
-
-},
-{ timestamps: true, toJSON:{virtuals: true}, toObject:{virtuals: true}}
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 ProductSchema.virtual('review', {
-    ref: 'Review',
-    localField: '_id',
-    foreignField: 'product',
-    justOne: false,
+  ref: 'Review',
+  localField: '_id',
+  foreignField: 'product',
+  justOne: false,
+});
 
-})
+ProductSchema.pre('deleteOne', async function (next) {
+  await this.model('Review').deleteMany({ product: this._id });
+});
 
-ProductSchema.pre('deleteOne', async function(next){
-    await this.model('Review').deleteMany({product: this._id})
-})
-
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('Product', ProductSchema);
